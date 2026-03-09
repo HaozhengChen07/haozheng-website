a7f1ad5892a0cf52af4d24a2ed78aedf0f789c17
Haozheng Chen <haozhengchen@HaozhengdeMacBook-Air.local>
2026-03-09 16:18:54 +0800

    Updates

--- _next/static/K_pb81Mc9gnv954dH8ItL/_buildManifest.js
+++ _next/static/K_pb81Mc9gnv954dH8ItL/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/haozheng-website//_next/:path+",
-        "destination": "/haozheng-website/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
